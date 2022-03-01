6d31f3514f51f5d9e6db33b84a1c5142fa043c90
BaKsPlayer <maxstepanov95@gmail.com>
2022-03-01 19:03:15 +0300

    Refactored MainSceneKnifeSpawner_System

---
+++ Assets/_Scripts/_MainScene/KnifeRotator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class KnifeRotator : MonoBehaviour
+{
+    private int _dir = 0;
+
+    private float _rotateSpeed = 250;
+
+    private Transform _transform;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _dir = (Random.Range(0, 2) == 0) ? -1 : 1;
+
+        _transform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        _transform.Rotate(Vector3.forward * _dir * _rotateSpeed * Time.deltaTime);          
+    }
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+
+        _transform.position = Vector3.zero;
+    }
+
+    public void SetRandomRotateSpeed(float min, float max)
+    {
+        _rotateSpeed = Random.Range(min, max);
+    }
+}
